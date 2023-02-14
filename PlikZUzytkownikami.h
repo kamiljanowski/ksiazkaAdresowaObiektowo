c766177ccec8508720e922d71f438fd7d74674cd
kamiljanowski <kamil.janowski.pl@gmail.com>
2023-02-14 23:23:05 +0100

    działa do konca trzeciego filmiku :)

--- PlikZUzytkownikami.h
+++ PlikZUzytkownikami.h
@@ -20,7 +20,7 @@ class PlikZUzytkownikami
     Uzytkownik pobierzDaneUzytkownika(string daneJednegoUzytkownikaOddzielonePionowymiKreskami);
 
 public:
-    PlikZUzytkownikami() : nazwaPlikuZUzytkownikami("Uzytkownicy.txt"){
+    PlikZUzytkownikami(string NAZWAPLIKUZUZYTKOWNIKAMI) : nazwaPlikuZUzytkownikami(NAZWAPLIKUZUZYTKOWNIKAMI){
         };
     void dopiszUzytkownikaDoPliku(Uzytkownik uzytkownik);
     vector <Uzytkownik> wczytajUzytkownikowZPliku();
